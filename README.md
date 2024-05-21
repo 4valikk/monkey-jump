fac356758c9dc74e2b97e666c892d2f53de2a8dc
4valikk <4valikk@chelsea.k12.mi.us>
2024-05-21 11:15:21 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,66 @@
+ 
+
+
+> Open this page at [https://cosmoscowboy.github.io/platform-jumper/](https://cosmoscowboy.github.io/platform-jumper/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/cosmoscowboy/platform-jumper** and import
+
+## Edit this project ![Build status badge](https://github.com/cosmoscowboy/platform-jumper/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/cosmoscowboy/platform-jumper** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/cosmoscowboy/platform-jumper/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://4valikk.github.io/monkey-jump/](https://4valikk.github.io/monkey-jump/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/4valikk/monkey-jump** and import
+
+## Edit this project ![Build status badge](https://github.com/4valikk/monkey-jump/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/4valikk/monkey-jump** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/4valikk/monkey-jump/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,175 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "brick1":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+d 1 d d d d d d d 1 d d d d d d 
+d 1 d d d d d d d 1 d d d d d d 
+d 1 d d d d d d d 1 d d d d d d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+d d d d d 1 d d d d d d d 1 d d 
+d d d d d 1 d d d d d d d 1 d d 
+d d d d d 1 d d d d d d d 1 d d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+d 1 d d d d d d d 1 d d d d d d 
+d 1 d d d d d d d 1 d d d d d d 
+d 1 d d d d d d d 1 d d d d d d 
+`;
+            case "image3":
+            case "brick2":return img`
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+`;
+            case "image4":
+            case "brick3":return img`
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+`;
+            case "image5":
+            case "brick4":return img`
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+1111111111111111
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+ddddd1ddddddd1dd
+1111111111111111
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+d1ddddddd1dddddd
+`;
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
